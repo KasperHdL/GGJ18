27d5d16942c6c4aa3db423fc47fed0506e505a45
Kasper Honnens de Lichtenberg <kasper@honnens.dk>
2018-01-26 22:01:20 +0100

    character control with controller

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -1,16 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using XInputDotNetPure;
 
 public class Character : MonoBehaviour {
 
-    public bool noController = true;
+    public bool use_keyboard = false;
+    public PlayerIndex playerIndex;
 
     public Vector2 steering;
 
     public float moveForce;
 
-    public Rigidbody body;
+    GamePadState state;
+    GamePadState prevState;
+
+    [HideInInspector] public Rigidbody body;
 	void Start () {
         body = GetComponent<Rigidbody>();
 		
@@ -18,9 +23,16 @@ public class Character : MonoBehaviour {
 	
 	void Update () {
 
-        if(noController){
+        if(use_keyboard){
             steering.x = Input.GetAxis("Horizontal");
             steering.y = Input.GetAxis("Vertical");
+        }else{
+            prevState = state;
+            state = GamePad.GetState(playerIndex);
+
+            steering.x = state.ThumbSticks.Left.X;
+            steering.y = state.ThumbSticks.Left.Y;
+
         }
 
         Vector3 move = Vector3.right * steering.x + Vector3.forward * steering.y;
@@ -28,4 +40,16 @@ public class Character : MonoBehaviour {
 
 		
 	}
+     void OnGUI()
+    {
+        string text = "Use left stick to turn the cube, hold A to change color\n";
+        text += string.Format("IsConnected {0} Packet #{1}\n", state.IsConnected, state.PacketNumber);
+        text += string.Format("\tTriggers {0} {1}\n", state.Triggers.Left, state.Triggers.Right);
+        text += string.Format("\tD-Pad {0} {1} {2} {3}\n", state.DPad.Up, state.DPad.Right, state.DPad.Down, state.DPad.Left);
+        text += string.Format("\tButtons Start {0} Back {1} Guide {2}\n", state.Buttons.Start, state.Buttons.Back, state.Buttons.Guide);
+        text += string.Format("\tButtons LeftStick {0} RightStick {1} LeftShoulder {2} RightShoulder {3}\n", state.Buttons.LeftStick, state.Buttons.RightStick, state.Buttons.LeftShoulder, state.Buttons.RightShoulder);
+        text += string.Format("\tButtons A {0} B {1} X {2} Y {3}\n", state.Buttons.A, state.Buttons.B, state.Buttons.X, state.Buttons.Y);
+        text += string.Format("\tSticks Left {0} {1} Right {2} {3}\n", state.ThumbSticks.Left.X, state.ThumbSticks.Left.Y, state.ThumbSticks.Right.X, state.ThumbSticks.Right.Y);
+        GUI.Label(new Rect(0, 0, Screen.width, Screen.height), text);
+    }
 }
