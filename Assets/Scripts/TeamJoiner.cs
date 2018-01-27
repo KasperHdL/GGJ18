5c7a7a1c301c092557af7361dc8e8379bb5a6cae
Kasper Honnens de Lichtenberg <kasper@honnens.dk>
2018-01-27 18:11:43 +0100

    some ui and some start game logic

--- Assets/Scripts/TeamJoiner.cs
+++ Assets/Scripts/TeamJoiner.cs
@@ -1,15 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TeamJoiner : MonoBehaviour {
 
+	private GameHandler gameHandler;
 	public Character.RoverType teamType;
+	public Text missingPlayers;
 
 	public Team team;
 
+	private int numPlayers;
+
 	// Use this for initialization
 	void Start () {
+		gameHandler = GameHandler.instance;
+		missingPlayers.text = 2-numPlayers + " Players Missing";
 		
 	}
 	
@@ -22,10 +29,17 @@ public class TeamJoiner : MonoBehaviour {
 		if(coll.tag == "Player"){
 			Character c = coll.GetComponent<Character>();
 			c.SetRoverType(teamType);
+			numPlayers++;
+			gameHandler.PlayerJoinedTeam(teamType);
+			missingPlayers.text = 2-numPlayers + " Players Missing";
 		}
 	}
 
 	void OnTriggerExit(Collider coll){
-
+		if(coll.tag == "Player"){
+			numPlayers--;
+			gameHandler.PlayerLeftTeam(teamType);
+			missingPlayers.text = 2-numPlayers + " Players Missing";
+		}
 	}
 }
