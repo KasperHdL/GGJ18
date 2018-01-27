776484e1de530841198991464b5b81e8c15ae9be
IYavin <itai.yavin@gmail.com>
2018-01-27 10:34:20 +0100

    WIP team script

---
+++ Assets/Scripts/Team.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Team : MonoBehaviour {
+	public Character player1;
+	public Character player2;
+
+	public Pattern pattern;
+	public Beam beam;
+
+
+}
