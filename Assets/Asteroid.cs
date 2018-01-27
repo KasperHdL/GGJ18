b63777daa634684db55f9bd7c5047c660e9300ce
Frans Peter Larsen <larsen.frans@gmail.com>
2018-01-27 22:38:57 +0100

    Random Asteroid Mesh w/ particles

---
+++ Assets/Asteroid.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Asteroid : MonoBehaviour {
+    public Mesh[] meshes;
+
+    public ParticleSystem ps; 
+
+	
+	void Start () {
+        MeshFilter meshFilter = GetComponent<MeshFilter>();
+        MeshCollider meshCollider = GetComponent<MeshCollider>();
+
+        int rnd = Random.Range(0, meshes.Length);
+        meshFilter.mesh = meshes[rnd];
+        meshCollider.sharedMesh = meshes[rnd];
+	}
+	
+	
+	void Update ()
+    {
+		
+	}
+}
